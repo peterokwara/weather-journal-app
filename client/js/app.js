8eaa3abe4d01b0e5fa72c9530b94fe174256156d
Peter Okwara <peter.okwara@outlook.com>
2021-10-30 23:12:14 +0300

    Initial commit

---
+++ client/js/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.use('')
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
