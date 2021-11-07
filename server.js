ec9584cae2dbf76c62f42f9316193afa218abdc6
Peter Okwara <peter.okwara@outlook.com>
2021-11-07 10:34:29 +0300

    Remove the config from the port, add some validation when posting data to the api

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-// import port settings
-const config = require("./data/config.local.json");
+// Specify the port in which the server will run in
+const port = 3000;
 
 // Setup empty JS object to act as endpoint for all routes
 const projectData = {
@@ -26,8 +26,8 @@ app.use(cors());
 app.use(express.static("client"));
 
 // Spin up the server
-const server = app.listen(config.port, () => {
-  console.log(`App is running on localhost:${config.port}`);
+const server = app.listen(port, () => {
+  console.log(`App is running on localhost:${port}`);
 });
 
 // Return all data in the object for get route
@@ -37,6 +37,16 @@ app.get("/all", (request, response) => {
 
 // Add data to the project object for post route
 app.post("/add", (request, response) => {
+  // If the request body is empty for temperature, throw an error
+  if (!request.body.temperature) {
+    throw new Error("Cannot store empty temperature on the projectData object");
+  }
+
+  // If the request body is empty for the date, throw an error
+  if (!request.body.date) {
+    throw new Error("Cannot store empty date on the projectData object");
+  }
+
   projectData["temperature"] = request.body.temperature;
   projectData["date"] = request.body.date;
   projectData["userResponse"] = request.body.userResponse;
